88c933da79c3cf94d690492da821e71430de4e00
EbunRuby <99331230+EbunRuby@users.noreply.github.com>
2022-03-22 02:43:32 +0800

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,13 @@
 #include "main.h"
-
 /**
- * reset_to_98 - takes a pointer to a parameter and updates its value.
- * @n: input integer.
- * Return: no return.
- */
+ * reset_to_98 - reset argument to 98
+ * @n: pointer
+ * Return: void
+*/
+
 void reset_to_98(int *n)
 {
-	*n = 98;
+
+*n = 98;
+
 }
