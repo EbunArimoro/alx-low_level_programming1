ccce573d208a556198cd119df61b148c2576c006
EbunRuby <99331230+EbunRuby@users.noreply.github.com>
2022-03-29 03:26:24 +0800

    Update 7-print_chessboard.c

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,21 +1,22 @@
 #include "main.h"
 /**
- *print_chessboard - prints the chessboard
- *@a:array input
- *
- *Return:void
- *
+ * *print_chessboard - description
+ * @a: board
+ * Return: void
  */
+
+
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
+int i, j;
 
-	for (i = 0; i < 8; i++)
+for (i = 0; i < 8; i++)
+{
+	for (j = 0; j < 8; j++)
 	{
-		for (j = 0; j < 8; j++)
-		{
-			_putchar(*(*(i + a) + j));
-		}
-		_putchar('\n');
+		_putchar(a[i][j]);
 	}
+	_putchar('\n');
+}
+
 }
